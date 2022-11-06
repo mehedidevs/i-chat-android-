3ffd106a0856fb4ccc842dc17fa0dfb1995bd129
Mehedi Hasan <mehedi.hasan.app.dev@gmail.com>
2022-11-06 22:07:07 +0600

    Fragment added

--- app/src/main/java/com/cit/i_chat/MainActivity.java
+++ app/src/main/java/com/cit/i_chat/MainActivity.java
@@ -1,14 +1,64 @@
 package com.cit.i_chat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
+        Toolbar toolbar = findViewById(R.id.mToolBar);
+        toolbar.setTitle("i-chat");
+        setSupportActionBar(toolbar);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+
+        menuInflater.inflate(R.menu.top_menu, menu);
+
+
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.logoutMenu:
+                mAuth.signOut();
+                startActivity(new Intent(MainActivity.this, StartActivity.class));
+                finish();
+
+                break;
+            case R.id.profileMenu:
+                Toast.makeText(this, "Profile Clickedl", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
